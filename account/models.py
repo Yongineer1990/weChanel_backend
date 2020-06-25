3595b2c26b1c095cd1efbbbf6771c9a62c992883
crystalyst <jy2908@columbia.edu>
2020-06-25 16:54:17 +0900

    Account Modeling #2

--- account/models.py
+++ account/models.py
@@ -1,30 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 class Account(models.Model):
-    email = models.CharField(max_length=100)
+    email      = models.EmailField(max_length=100, unique=True)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    password = models.CharField(max_length=1000)
+    last_name  = models.CharField(max_length=50)
+    password   = models.CharField(max_length=1000)
 
-    # To manually create the name of db table above:
     class Meta:
-        db_table = 'account'
-
-#class Product_wishlists(models.Model):
-    #this wishlist has two foreign keys, which are 1. product and 2. account
-#   product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-
-#   class Meta:
-#        db_table = 'product_wishlists'
-
-#class Look_wishlists(models.Model):
-    #this wishlist has two foreign keys, which are 1. look and 2. account
-#    look = models.ForeignKey(Look, on_delete=models.CASCADE)
-#    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-
-#    class Meta:
-#        db_table = 'look_wishlists'
-
+        db_table = 'accounts'
