1d949a6e86fefa8bb860673b7db33a3a7deff621
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-06-25 20:03:53 +0900

    products app의 models.py 작성 완료

--- products/models.py
+++ products/models.py
@@ -2,39 +2,46 @@ from django.db import models
 
 # Create your models here.
 
-class Menus(models.Model):
+class Menu(models.Model):
     name = models.CharField(max_length=50, null=True)
 
     class Meta:
         db_table = 'menus'
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
-    menu = models.ForeignKey('Menus', on_delete=models.SET_NULL, null=False)
+    menu = models.ForeignKey('Menu', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'categories'
 
-class Collections(models.Model):
+class Collection(models.Model):
     name = models.CharField(max_length=100, null=True)
 
     class Meta:
         db_table = 'collections'
 
-class Looks(models.Model):
+class Look(models.Model):
     name = models.CharField(max_length=50, null=True)
-    collection = models.ForeignKey('Collections', on_delete=models.SET_NULL, null=False)
-    product = models.ForeignKey('Products', on_delete=models.SET_NULL, null=False)
+    collection = models.ForeignKey('Collection', on_delete=models.CASCADE)
+    product = models.ManyToManyField('Product', through='Product_look', related_name='product')
 
     class Meta:
         db_table = 'looks'
 
-class Products(models.Model):
+class Product(models.Model):
     product_code = models.CharField(max_length=100, null=True)
     name = models.CharField(max_length=50, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=6, null=True)
-    category = models.ForeignKey('Categories', on_delete=models.SET_NULL, null=False)
-    collection = models.ForeignKey('Collections', on_delete=models.SET_NULL, null=False)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    collection = models.ForeignKey('Collection', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'products'
+
+class Product_look(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    look = models.ForeignKey('Look', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'products_looks'
