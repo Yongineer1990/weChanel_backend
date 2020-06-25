8d8e95d71b486ba8c566308de2c695b08352e4bc
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-06-25 20:29:18 +0900

    products app의 models.py 수정

--- products/models.py
+++ products/models.py
@@ -10,7 +10,7 @@ class Menu(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
-    menu = models.ForeignKey('Menu', on_delete=models.SET_NULL, null=True)
+    menu = models.ForeignKey(Menu, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'categories'
@@ -21,27 +21,27 @@ class Collection(models.Model):
     class Meta:
         db_table = 'collections'
 
-class Look(models.Model):
-    name = models.CharField(max_length=50, null=True)
-    collection = models.ForeignKey('Collection', on_delete=models.CASCADE)
-    product = models.ManyToManyField('Product', through='Product_look', related_name='product')
-
-    class Meta:
-        db_table = 'looks'
-
 class Product(models.Model):
     product_code = models.CharField(max_length=100, null=True)
     name = models.CharField(max_length=50, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=6, null=True)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-    collection = models.ForeignKey('Collection', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'products'
 
+class Look(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, through='Product_look', related_name='product')
+
+    class Meta:
+        db_table = 'looks'
+
 class Product_look(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    look = models.ForeignKey('Look', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    look = models.ForeignKey(Look, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products_looks'
