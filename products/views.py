b7cfa9dd5bb2e7c5c2cd6d3ad57b434363c59421
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-06-30 15:24:08 +0900

    룩 상세정보에서 예외처리 추가

--- products/views.py
+++ products/views.py
@@ -24,41 +24,49 @@ class AllLook(View):
 
 class LookDetail(View):
     def post(self, request, look_num):
-        look_detail = []
-        looks       = Look.objects.all().prefetch_related('product')
-        look        = looks.get(id=look_num)
-        products    = look.product
+        try:
+            look_detail = []
+            looks       = Look.objects.all().prefetch_related('product')
+            look        = looks.get(id=look_num)
+            products    = look.product
 
-        for product in products.all():
-            print(product.product_code)
-            product_info    = []
-            color_info      = []
-            texture_info    = []
+            for product in products.all():
+                print(product.product_code)
+                product_info    = []
+                color_info      = []
+                texture_info    = []
 
-            colors = product.color.all()
-            for color in colors:
-                color_info.append(color.name)
+                colors = product.color.all()
+                for color in colors:
+                    color_info.append(color.name)
 
-            textures = product.texture.all()
-            for texture in textures:
-                texture_info.append(texture.name)
+                textures = product.texture.all()
+                for texture in textures:
+                    texture_info.append(texture.name)
 
-            product_info.append({
-                'Product_code'  : product.product_code,
-                'Name'          : product.name,
-                'price'         : product.price,
-                'color'         : color_info,
-                'texture'       : texture_info
+                product_info.append({
+                    'Product_code'  : product.product_code,
+                    'Name'          : product.name,
+                    'price'         : product.price,
+                    'color'         : color_info,
+                    'texture'       : texture_info
 
-            })
-            look_detail.append(product_info)
+                })
+                look_detail.append(product_info)
 
-        look_images = LookImage.objects.filter(look_id=look_num)
-        look_image  = []
-        for image in look_images:
-            look_image.append(image.url)
+            look_images = LookImage.objects.filter(look_id=look_num)
+            look_image  = []
+            for image in look_images:
+                look_image.append(image.url)
 
-        return JsonResponse({
-            'img'       : look_image,
-            'products'   : look_detail,
-        }, status=200)
+            return JsonResponse({
+                'img'       : look_image,
+                'products'   : look_detail,
+            }, status=200)
+
+        except KeyError as e:
+            return JsonResponse({'Message': f"KEY ERROR! : {e}"}, status=400)
+
+        except Look.DoesNotExist:
+            return JsonResponse({'Message': f'잘못된 접근 - look_num : {look_num}'}, status=400)
+
