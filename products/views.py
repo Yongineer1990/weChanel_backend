915087f03e251eee2ffce17f46ed9013f8aaa285
고혁훈 <khh180cm@gohyeoghun-ui-MacBookAir.local>
2020-06-30 19:35:12 +0900

    products앱 views.py의 DetailView 수정- 샤넬 백 옵션 개수 및 제품 번호 리턴 기능 추가

--- products/views.py
+++ products/views.py
@@ -16,22 +16,24 @@ class BagView(View):
                                                            ).exclude(id__lt=400)
 
         for bag in bag_list:
-            bag_code            = bag.product_code
-            bag_img             = bag.productimage_set.all()
-            bag_img         = bag_img[0].url
-            bag_name        = bag.name
-            bag_price       = bag.price
-            each_bag        = Product.objects.get(product_code=bag_code)
-            textures        = each_bag.texture.all()
-            texture_list    = []
-            bag_info = {}
+            bag_code                = bag.product_code
+            bag_img                 = bag.productimage_set.all()
+            bag_img                 = bag_img[0].url
+            bag_name                = bag.name
+            bag_price               = bag.price
+            each_bag                = Product.objects.get(product_code=bag_code)
+            textures                = each_bag.texture.all()
+            texture_list            = []
+            bag_info                = {}
             for texture in textures:
-                texture = texture.name
+                texture             = texture.name
                 texture_list.append(texture)
+
             bag_info['bag_img']     = bag_img
             bag_info['bag_name']    = bag_name
             bag_info['bag_price']   = bag_price
             bag_info['texture']     = texture_list
+            bag_info['bag_code']    = bag_code.replace(' ','')
             total_bag_info.append(bag_info)
 
         return JsonResponse({'bag_info':total_bag_info}, status=200)
@@ -40,7 +42,7 @@ class BagView(View):
 
 class DetailView(View):
     def get(self, request, bag_code):
-        print(bag_code)
+        print('입력받은 코드  ', bag_code)
         detail_bag_info = {}
         product_list = Product.objects.prefetch_related(
                                                         'productimage_set',
@@ -53,9 +55,13 @@ class DetailView(View):
                                                                 ).exclude(
                                                                             id__lt=400
                                                                          )
-        bag_num -= 1
+
+
+
+        code_list = [ product.product_code.replace(' ','') for product in product_list ]
         try:
-            bag      = product_list[bag_num]
+            index = code_list.index(bag_code)
+            bag                 = product_list[index]
             bag_image_all       = [ image.url for image in bag.productimage_set.all() ]
             bag_texture         = [ texture.name for texture in bag.texture.all() ]
             bag_size_main       = bag.sizeproduct_set.all()[0].size.size_main
@@ -67,19 +73,23 @@ class DetailView(View):
             leather_bag_images  = []
             tweed_bag_images    = []
             other_bag_images    = []
+            leather_bag_codes   = []
+            tweed_bag_codes     = []
+            other_bag_codes     = []
 
-            leather_bags    =  Product.objects.filter(
-                                                        meterial__name='Leather'
-                                                     ).filter(name=bag.name)
+            leather_bags        =  Product.objects.filter(
+                                                            meterial__name='Leather'
+                                                         ).filter(name=bag.name)
             if leather_bags.exists():
                 leather_bag_images = [ leather_bag.productimage_set.all()[0].url for leather_bag in leather_bags ]
-
+                leather_bag_codes  =  [leather.product_code.replace(' ','') for leather in leather_bags ]
 
             tweed_bags      = Product.objects.filter(
                                                         meterial__name='트위드 & 패브릭'
                                                     ).filter(name=bag.name)
             if tweed_bags.exists():
                 tweed_bag_images = [ tweed_bag.productimage_set.all()[0].url for tweed_bag in tweed_bags ]
+                tweed_bag_codes  = [ tweed.product_code.replace(' ','') for tweed in tweed_bags ]
 
 
             other_bags      = Product.objects.filter(
@@ -87,6 +97,7 @@ class DetailView(View):
                                                     ).filter(name=bag.name)
             if other_bags.exists():
                 other_bag_images = [ other_bag.productimage_set.all()[0].url for other_bag in other_bags ]
+                other_bag_codes  = [ other.product_code.replace(' ','') for other in other_bags ]
 
             detail_bag_info['bag_image_all']        = bag_image_all
             detail_bag_info['bag_texture']          = bag_texture
@@ -97,9 +108,11 @@ class DetailView(View):
             detail_bag_info['bag_price']            = bag_price
             detail_bag_info['bag_name']             = bag_name
             detail_bag_info['leather_bag_images']   = leather_bag_images
+            detail_bag_info['leather_bag_codes']    = leather_bag_codes
             detail_bag_info['tweed_bag_images']     = tweed_bag_images
+            detail_bag_info['tweed_bag_codes']      = tweed_bag_codes
             detail_bag_info['other_bag_images']     = other_bag_images
-
+            detail_bag_info['other_bag_codes']      = other_bag_codes
             return JsonResponse({'detail_bag_info':detail_bag_info}, status=200)
 
         except IndexError:
