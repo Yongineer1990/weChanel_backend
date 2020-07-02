254e799411866d1290ec436058fc790418cdd31f
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-07-02 21:06:01 +0900

    product_info를 list comprehension으로 변환

--- products/views.py
+++ products/views.py
@@ -29,32 +29,46 @@ class LookDetail(View):
     def get(self, request, look_num):
         try:
             if Look.objects.filter(id=look_num).exists():
-                looks       = Look.objects.get(id=look_num)
+                looks       = Look.objects.prefetch_related('product').get(id=look_num)
                 products    = looks.product.all()
 
-                product_info = []
-                for product in products:
+                product_info = [{
+                    'product_id'    : product.id,
+                    'product_name'  : product.name,
+                    'product_code'  : product.product_code,
+                    'product_price' : product.price,
+                    'color'         : [{
+                        'color_id'      : color.id,
+                        'color_name'    : color.name
+                    } for color in product.color.all()],
+                    'texture'       : [{
+                        'texture_id'    : texture.id,
+                        'texture_name'  : texture.name
+                    } for texture in product.texture.all()]
+                } for product in products]
+                # product_info = []
+                # for product in products:
 
-                    colors = Color.objects.filter(product=product.id)
-                    color_info = [{
-                        'id'    : color.id,
-                        'name'  : color.name
-                    } for color in colors]
+                    # colors = Color.objects.filter(product=product.id)
+                    # color_info = [{
+                        # 'id'    : color.id,
+                        # 'name'  : color.name
+                    # } for color in colors]
 
-                    textures = Texture.objects.filter(product=product.id)
-                    texture_info = [{
-                        'id'    : texture.id,
-                        'name'  : texture.name
-                    } for texture in textures]
+                    # textures = Texture.objects.filter(product=product.id)
+                    # texture_info = [{
+                        # 'id'    : texture.id,
+                        # 'name'  : texture.name
+                    # } for texture in textures]
 
-                    product_info.append({
-                        'product_id'    : product.id,
-                        'product_name'  : product.name,
-                        'product_code'  : product.product_code,
-                        'product_price' : product.price,
-                        'color'         : color_info,
-                        'texture'       : texture_info
-                    })
+                    # product_info.append({
+                        # 'product_id'    : product.id,
+                        # 'product_name'  : product.name,
+                        # 'product_code'  : product.product_code,
+                        # 'product_price' : product.price,
+                        # 'color'         : color_info,
+                        # 'texture'       : texture_info
+                    # })
 
                 look_images = LookImage.objects.filter(look_id=look_num)
                 look_image  = [image.url for image in look_images]
