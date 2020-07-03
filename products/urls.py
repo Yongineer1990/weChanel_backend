9f6a9cbb1ed16e4208e847f375ecd06aa575d0c0
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-07-03 14:28:14 +0900

    products/urls.py bugfix

--- products/urls.py
+++ products/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('/cruise-2019-20', AllLook.as_view()),
     path('/cruise-2019-20/<int:look_num>', LookDetail.as_view()),
     path('/wishlist/look/<int:look_id>', LookWishlist.as_view()),
-    path('/wishlist/prod/<int:product_id>', ProductWishlist.as_view())
+    path('/wishlist/prod/<int:product_id>', ProductWishlist.as_view()),
     path('/chanel-19', BagView.as_view()),
     path('/chanel-19/detail/<slug:query_bag_code>', BagDetail.as_view()),
 ]
